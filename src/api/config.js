030f52b9542307f90cfe3fe2ca4730c311dbfbc2
CodeMeKathy * <Kathy@KathyLambert.me>
2023-01-31 00:04:18 -0500

    Update firebase config file

--- src/api/config.js
+++ src/api/config.js
@@ -3,15 +3,33 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'FILL_ME_IN',
-	authDomain: 'FILL_ME_IN',
-	databaseURL: 'FILL_ME_IN',
-	projectId: 'FILL_ME_IN',
-	storageBucket: 'FILL_ME_IN',
-	messagingSenderId: 'FILL_ME_IN',
-	appId: 'FILL_ME_IN',
+	apiKey: 'AIzaSyAifabxru17BiZ6Nt6v0sVvRtB83GHOcMQ',
+	authDomain: 'shoppinglistapp-783fc.firebaseapp.com',
+	projectId: 'shoppinglistapp-783fc',
+	storageBucket: 'shoppinglistapp-783fc.appspot.com',
+	messagingSenderId: '831850332087',
+	appId: '1:831850332087:web:5a3500e0edf8e3f557f232',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+// **Provided** via Firebase
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAifabxru17BiZ6Nt6v0sVvRtB83GHOcMQ",
+//   authDomain: "shoppinglistapp-783fc.firebaseapp.com",
+//   projectId: "shoppinglistapp-783fc",
+//   storageBucket: "shoppinglistapp-783fc.appspot.com",
+//   messagingSenderId: "831850332087",
+//   appId: "1:831850332087:web:5a3500e0edf8e3f557f232"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
