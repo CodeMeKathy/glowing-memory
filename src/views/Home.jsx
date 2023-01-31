48f67588dca080d623feafe3472ae28e4221e886
CodeMeKathy * <Kathy@KathyLambert.me>
2023-01-31 17:14:56 -0500

    Refactor (<Home/>): Add comments

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,19 @@
+// React Components Imports
+import { useEffect } from 'react';
+import { redirect } from 'react-router-dom';
+
+// Custom Components Imports
 import './Home.css';
 
-export function Home() {
+export function Home(listToken) {
+	useEffect(() => {
+		if (listToken) {
+			console.log(listToken);
+			redirect('/list');
+		} else {
+			return redirect('/');
+		}
+	}, [listToken]);
 	return (
 		<div className="Home">
 			<p>
