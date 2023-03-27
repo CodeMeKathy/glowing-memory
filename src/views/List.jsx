88903b69766ec7233a681e1bd915794d587bacce
CodeMeKathy * <Kathy@KathyLambert.me>
2023-03-27 16:51:27 -0400

    Feat (<List/>) : Render shopping list items

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,14 +1,47 @@
+// React Components
+import { useEffect } from 'react';
 import { ListItem } from '../components';
 
+//* Access and Render User's ShoppingList
 export function List({ data }) {
+	console.log({ data });
+	let listArray = data;
+	console.log({ listArray });
+
 	return (
 		<>
-			<p>
+			<h2>
 				Hello from the <code>/list</code> page!
-			</p>
+			</h2>
+			<p></p>
 			<ul>
+				{/* If the imported data array length is not greater than zero (0), i.e. is empty, render a message to the user else render the `<ListItem/>` component */}
+				{Object.values(listArray).length > 0 ? (
+					<ul>
+						{Object.values(listArray).map((listArrayItem, i) => {
+							return (
+								<>
+									<li>
+										<ListItem
+											key={`${listArrayItem.id}_${i}`}
+											name={listArrayItem.name}
+										/>
+									</li>
+								</>
+							);
+						})}
+					</ul>
+				) : (
+					<>
+						<p>
+							Your list contains {Object.values(listArray).length} items and is
+							currently empty.
+						</p>
+						<p>Please add some items to your list!</p>
+					</>
+				)}
 				{/**
-				 * TODO: write some JavaScript that renders the `data` array
+				 * - [✅] TODO: write some JavaScript that renders the `data` array
 				 * using the `ListItem` component that's imported at the top
 				 * of this file.
 				 */}
