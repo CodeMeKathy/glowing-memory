e732a712f113af27efb389ee4525f83970fc0964
CodeMeKathy * <Kathy@KathyLambert.me>
2023-03-27 16:47:24 -0400

    Refactor (<App/>): Connect firebase data and pass to <List/>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+// React Components Imports
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { redirect } from 'react-router-dom';
 
+// Custom Components Imports
 import { AddItem, Home, Layout, List } from './views';
 
+// Custom Utilities Imports
 import { getItemData, streamListItems } from './api';
 import { useStateWithStorage } from './utils';
 
@@ -13,13 +17,13 @@ export function App() {
 	 * that can be used to update `listToken` later.
 	 *
 	 * `listToken` is `my test list` by default so you can see the list
-	 * of items that was prepopulated for this project.
-	 * You'll later set it to `null` by default (since new users do not
-	 * have tokens), and use `setListToken` when you allow a user
-	 * to create and join a new list.
+	 * of items that was pre-populated for this project.
+	 *
+	 * 	// TODO: You'll later set it to `null` by default (since new users do not have tokens), and use `setListToken` when you allow a user to create and join a new list.
 	 */
 	const [listToken, setListToken] = useStateWithStorage(
-		'my test list',
+		// null,
+		'my test list', //Add as a new collection within Firebase to manual add and view items
 		'tcl-shopping-list-token',
 	);
 
@@ -27,7 +31,7 @@ export function App() {
 		if (!listToken) return;
 
 		/**
-		 * streamListItems` takes a `listToken` so it can commuinicate
+		 * streamListItems` takes a `listToken` so it can communicate
 		 * with our database, then calls a callback function with
 		 * a `snapshot` from the database.
 		 *
@@ -35,8 +39,7 @@ export function App() {
 		 */
 		return streamListItems(listToken, (snapshot) => {
 			/**
-			 * Here, we read the documents in the snapshot and do some work
-			 * on them, so we can save them in our React state.
+			 * 	// TODO: read the documents in the snapshot and do some work on them, so we can save them in our React state.
 			 *
 			 * Refer to `api/firebase.js`
 			 */
@@ -51,9 +54,21 @@ export function App() {
 		<Router>
 			<Routes>
 				<Route path="/" element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path="/list" element={<List data={data} />} />
-					<Route path="/add-item" element={<AddItem />} />
+					<Route
+						index
+						element={<Home setListToken={setListToken} listToken={listToken} />}
+					/>
+					<Route
+						path="/list"
+						element={
+							<List
+								listToken={listToken}
+								data={data}
+								// listToken={listToken}
+							/>
+						}
+					/>
+					<Route path="/add-item" element={<AddItem listToken={listToken} />} />
 				</Route>
 			</Routes>
 		</Router>
